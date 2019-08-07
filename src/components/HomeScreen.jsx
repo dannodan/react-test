5116be22ab59e519ce1f642ba9bde31e0c71c77b
Dan no Dan <dannodan89@gmail.com>
2019-08-07 16:00:50 -0400

    Created Home Screen

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { PageHeader, Button } from 'antd';
+
+export default class HomeScreen extends Component {
+
+    selectUser = (user) => () => {
+        const { selectUser } = this.props;
+        selectUser(user);
+    }
+
+    renderUsers = () => {
+        const topFive = [
+            "GrahamCampbell", "fabpot", "weierophinney", "rhk", "josh"
+        ]
+
+        return topFive.map((entry, index) => <Button key={`user-${index}`} onClick={this.selectUser(entry)}>{entry}</Button>);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <PageHeader title="Home" backIcon={false}/>
+                <h1>Top 5 GitHub Users</h1>
+                <p>Tap the username to see more information</p>
+                {this.renderUsers()}
+            </React.Fragment>
+        )
+    }
+}
